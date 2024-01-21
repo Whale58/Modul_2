224b2dfbe0aa27c6738faee2e90cef3c2a66668b
Whale58 <whalew85@gmail.com>
2024-01-21 16:20:49 +0300

    Вывел несколько символьных литералов

--- Modul_2.ConsoleApp/Program.cs
+++ Modul_2.ConsoleApp/Program.cs
@@ -12,6 +12,8 @@ namespace Modul_2.ConsoleApp
 
             Console.WriteLine("Привет Мир!");
             Console.WriteLine("Мне 25");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0023"); 
 
             Console.ReadKey();
         }
