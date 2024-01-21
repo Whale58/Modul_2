9b6ccd298115a4677a8970c4b7f9ba061f7667ff
Whale58 <whalew85@gmail.com>
2024-01-21 16:36:57 +0300

    Создал ветку Dev для разработки

--- Modul_2.ConsoleApp/Program.cs
+++ Modul_2.ConsoleApp/Program.cs
@@ -15,7 +15,7 @@ namespace Modul_2.ConsoleApp
             Console.WriteLine("MyAge " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIApet);
             Console.WriteLine("My shoe size is " + MyShoeSize);
-
+            
             Console.ReadKey();
         }
     }
