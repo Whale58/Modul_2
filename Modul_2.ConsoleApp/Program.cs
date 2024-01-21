3cba242a8cd158ee827c04e2e510c76b65527916
Whale58 <whalew85@gmail.com>
2024-01-21 16:23:25 +0300

    Выполнил задание 3.3.2

--- Modul_2.ConsoleApp/Program.cs
+++ Modul_2.ConsoleApp/Program.cs
@@ -7,13 +7,14 @@ namespace Modul_2.ConsoleApp
         static void Main(string[] args)
         {
             const String MyName = "Виталий";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
 
-            Console.WriteLine(MyName);
-
-            Console.WriteLine("Привет Мир!");
-            Console.WriteLine("Мне 25");
-            Console.WriteLine("\t My name is \n {0}", MyName);
-            Console.WriteLine("\u0023"); 
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
 
             Console.ReadKey();
         }
