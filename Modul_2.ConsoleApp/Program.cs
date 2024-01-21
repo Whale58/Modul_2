d3ae11ca798ff6ad69cdd5d169c31e4926204fc6
Whale58 <whalew85@gmail.com>
2024-01-21 10:00:38 +0300

    Добавьте файлы проекта.

---
+++ Modul_2.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul_2.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String MyName;
+            MyName = "Виталий";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
