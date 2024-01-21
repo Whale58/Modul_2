0c045863149fabe5d058d042d19f129b509c97e7
Whale58 <whalew85@gmail.com>
2024-01-21 20:32:11 +0300

    Узнаем возраст, добавляем любимый день недели (преобразование типов)

--- Modul_2.ConsoleApp/Program.cs
+++ Modul_2.ConsoleApp/Program.cs
@@ -6,25 +6,16 @@ namespace Modul_2.ConsoleApp
     {
         static void Main(string[] args)
         {
-			string MyName = "Евгения";
-			Console.WriteLine("Привет, мир");
-			Console.WriteLine();
-			Console.WriteLine("Меня зовут {0}", MyName);
-
-			//0 — позиция нашей переменной. То есть переменная будет на позиции 0.
-
-
-			//Также мы можем вставить в строку не переменную, а строковый литерал:
-			Console.WriteLine("Меня зовут {0}", "Маша");
-
-
-			//И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
-			var age = 27;
-			var weight = 50;
-			Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
-
-			Console.ReadKey();
-		}
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Ваш любимый день недели?");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.WriteLine("Ваш любимый день недели {0}", day);
+            Console.ReadKey();
+        }
 
     }
 }
